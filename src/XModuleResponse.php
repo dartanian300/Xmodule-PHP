55aaff372e9004511d90d7e3735077f98465774d
Leb <cmhardin@bellsouth.net>
2019-07-18 09:00:37 -0400

    Implement JsonSerializable interface

--- src/XModuleResponse.php
+++ src/XModuleResponse.php
@@ -1,24 +1,65 @@
 <?php
-class XModuleResponse {
-	protected $metadata;	// array
-	protected $content;	// array
-	protected $regionContent;	// array
-	public static function constructor__ () 
+require_once(__DIR__."/Traits/ModifiableArray.php");
+
+/**
+ *  Summary.
+ *  Description.
+ *  @method void add(mixed $item)
+ *  @method mixed get(integer $position = null)
+ *  @method void delete(integer $position)
+ *
+ *  @todo: figure out why these methods aren't parsing
+ */
+class XModuleResponse implements JsonSerializable {
+    use ModifiableArray; 
+    
+    /** @var string[] Associative array of metadata properties */
+	private $metadata;
+    /** @var mixed[] Elements that should be included in the Xmodule response */
+	private $content;
+    /** @var mixed[] Same as $content, but for AJAX content */
+	private $regionContent;
+    
+	public function __construct() 
 	{
-		$me = new self();
-		parent::constructor__();
-		return $me;
+        $this->setModifiableProperties(array('content'));
+        
+		$this->metadata = array();
+        $this->content = array();
+        $this->regionContent = array();
+        
+        $this->initializeMetadata();
 	}
-	abstract function XModuleBase (); 
-	public function print () 
+        
+    /**
+     *  Creates JSON response for xmodule
+     *  @return string JSON representation of object
+     */
+	public function print() 
 	{
-		return "";
+        return json_encode($this);
 	}
-	abstract function add ($item); // [mixed item]
-	public function get ($position) // [int position]
-	{
-		return NULL;
-	}
-	abstract function delete ($position); // [int position]
+    
+    public function jsonSerialize()
+    {        
+        $format = array(
+            'metadata' => $this->metadata,
+            'content' => $this->content
+        );
+        
+        return $format;
+    }
+    
+    /**
+     *  Sets default metadata 
+     *  @internal
+     */
+    private function initializeMetadata()
+    {
+        $defaultMeta = array(
+            "version" => "1"
+        );
+        $this->metadata = array_merge($this->metadata, $defaultMeta);
+    }
 }
-?>
+
