dcafb51fdf721ce38b5c66a910edcbd6623bbc96
Leb <cmhardin@bellsouth.net>
2019-07-18 15:21:28 -0400

    Make private properties protected

--- src/DataWrappers/Number.php
+++ src/DataWrappers/Number.php
@@ -1,13 +1,48 @@
 <?php
+require_once(__DIR__."/DataWrapperBase.php");
+
 class Number extends DataWrapperBase {
-	protected $min;	// number
-	protected $max;	// number
-	public static function constructor__ () 
+    /** @var int The minimum this number can be. Should be set by subclasses */
+	protected $min = null;
+    /** @var int The maximum this number can be. Should be set by subclasses */
+	protected $max = null;
+    
+	public function __construct() 
 	{
-		$me = new self();
-		parent::constructor__();
-		return $me;
+		parent::__construct();
+        
+        $this->set(-1);
 	}
-	abstract function set ($num); // [number num]
+    
+    /**
+     *  @param int|float $num
+     *
+     *  @throws OutOfRangeException if the provided argument is not within the ranges set by min and max
+     */
+	public function set($num){
+        
+        if ($this->min != null && $this->max != null){
+            // enforce min & max
+            if ($num > $this->min && $num < $this->max)
+                $this->data = $num;
+            else
+                throw new OutOfRangeException("Parameter must be between $this->min and $this->max.");
+        } else if ($this->min != null){
+            // enforce min
+            if ($num > $this->min)
+                $this->data = $num;
+            else
+                throw new OutOfRangeException("Parameter must be larger than $this->min.");
+        } else if ($this->max != null){
+            // enforce max
+            if ($num < $this->max)
+                $this->data = $num;
+            else
+                throw new OutOfRangeException("Parameter must be smaller than $this->max.");
+        } else {
+            // no enforcement
+            $this->data = $num;
+        }
+    }
 }
-?>
+
