7000dca55b7e7eb0d210947deacc5e0d25e16d50
Leb <dartanian300@users.noreply.github.com>
2019-07-23 13:28:30 -0400

    use JsonSerializable interface

--- src/DataWrappers/Image.php
+++ src/DataWrappers/Image.php
@@ -4,7 +4,7 @@ namespace XModule\DataWrapper;
 require_once(__DIR__."/XString.php");
 require_once(__DIR__."../Helpers/Badge.php");
 
-class Image extends XString {
+class Image extends XString implements \JsonSerializable {
     /**
      *  @var Badge Used in GridItem
      *  @see Badge
@@ -16,5 +16,15 @@ class Image extends XString {
 		parent::__construct();
         $this->badge = new Badge();
 	}
+    
+    public function jsonSerialize()
+    {        
+        $format = array(
+            'url' => $this->data,
+            'badge' => $this->badge
+        );
+        
+        return $format;
+    }
 }
 
