eb8e8c83cb4ecb60f2decdf5eba3253d7a9d4705
Leb <dartanian300@users.noreply.github.com>
2019-07-24 09:37:04 -0400

    force hex format

--- src/DataWrappers/Color.php
+++ src/DataWrappers/Color.php
@@ -6,10 +6,10 @@ require_once(__DIR__."/XString.php");
 /**
  *  @todo Enforce color format (hex)
  */
-class Color extends String {
-	public function __construct() 
+class Color extends XString {
+	public function __construct($color) 
 	{
-		parent::__construct();
+		parent::__construct($color, '/#[0-9a-z]{6}/');
 	}
 }
 
