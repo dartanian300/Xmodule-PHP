c1a2eb7992cc6c8e7f99a226df4c32fd1b7b1d4f
Leb <dartanian300@users.noreply.github.com>
2019-08-14 08:34:13 -0400

    add default color

--- src/DataWrappers/Color.php
+++ src/DataWrappers/Color.php
@@ -4,7 +4,7 @@ namespace XModule\DataWrappers;
 require_once(__DIR__."/XString.php");
 
 class Color extends XString {
-	public function __construct($color) 
+	public function __construct($color = '#000000') 
 	{
 		parent::__construct($color, '/^#[0-9a-f]{6}$/');
 	}
