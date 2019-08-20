9a1085597ea868c47bf14d8999792e714668cea7
Leb <dartanian300@users.noreply.github.com>
2019-08-20 11:48:37 -0400

    typos

--- src/MultiColumn.php
+++ src/MultiColumn.php
@@ -51,16 +51,37 @@ class MultiColumn extends Element implements \JsonSerializable {
     /**
      *  @param int $columnNum The column to add the item to (0-indexed)
      *  @param mixed $item The object to add to that column
+     *  @return int Index of array where $item was added
      */
 	public function add($columnNum, $item)
     {
-        if ($columnNum > $this->numColumns->get() - 1 || $columnNum < 0)
+        if ($columnNum > $this->getNumColumns() - 1 || $columnNum < 0)
             throw new OutOfBoundsException('The parameter $columnNum must be less than or equal to the number of columns for this object');
         
-        if (gettype($this->columns[$numColumns]) != "array")
-            $this->columns[$numColumns - 1] = array();
+        if (gettype($this->columns[$columnNum]) != "array")
+            $this->columns[$columnNum] = array();
         
-        $this->columns[$numColumns - 1][] = $item;
+        $this->columns[$columnNum][] = $item;
+        
+        // move pointer to end of array to return correct key
+        end($this->columns[$columnNum]);
+//        var_dump($this->columns);
+        return key($this->columns[$columnNum]);
+    }
+    
+    /**
+     *  @param int $columnNum The column to fetch the element from (0-indexed)
+     *  @param int $elementIndex The index to fetch (0-indexed)
+     *  @return mixed Element at $elementIndex
+     */
+	public function get($columnNum, $elementIndex)
+    {
+        if ($columnNum > $this->getNumColumns() - 1 || $columnNum < 0)
+            throw new OutOfBoundsException('The parameter $columnNum must be less than or equal to the number of columns for this object AND above -1');
+        if ($elementIndex > count($this->columns[$columnNum]) - 1 || $elementIndex < 0)
+            throw new OutOfBoundsException('The parameter $elementIndex must be less than or equal to the number of elements in the array AND above -1. $elementIndex: '.$elementIndex.'  Array Length: '.count($this->columns[$columnNum]));
+        
+        return $this->columns[$columnNum][$elementIndex];
     }
     
     public function jsonSerialize()
