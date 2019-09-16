02fc19d3687ae64d8fb54c681d79ce3c112ec123
Caleb Hardin <dartanian300@users.noreply.github.com>
2019-09-16 15:54:02 -0400

    add id field

--- src/Helpers/XModule.php
+++ src/Helpers/XModule.php
@@ -5,18 +5,22 @@ use XModule\DataWrappers as DataWrappers;
 
 class XModule implements \JsonSerializable {
     /** @var XString */
+	public $id;
+    /** @var XString */
 	public $relativePath;
     
 	public function __construct() 
 	{
 //		parent::__construct();
         
+        $this->id = new DataWrappers\XString();
         $this->relativePath = new DataWrappers\XString();
 	}
     
     public function jsonSerialize()
     {        
         $format = array(
+            'id' => $this->id,
             'relativePath' => $this->relativePath,
         );
         
